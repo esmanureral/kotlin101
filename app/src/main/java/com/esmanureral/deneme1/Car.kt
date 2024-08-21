ce26cb50f23b49b424b2218ae1243942f9911ee7
Esmanur <esmanur2eral@gmail.com>
2024-08-21 19:32:14 +0300

    101 kotlin

---
+++ app/src/main/java/com/esmanureral/deneme1/Car.kt
@@ -0,0 +1,15 @@
+package com.esmanureral.deneme1
+
+//constructor ile oluşturduk.
+class Car (brand:String,model:Long,color:String){
+    var brand=brand
+        private set
+    var model=model
+        private set
+    var color=color
+        private set
+
+    //get:okumak,set:değiştirmek ENCAPSULATİON
+    //dışarıdan değiştirilmesin diye private set yaptık.
+
+}
---
+++ app/src/main/java/com/esmanureral/deneme1/Sum.kt
@@ -0,0 +1,6 @@
+package com.esmanureral.deneme1
+
+class Sum {
+
+    fun mySum(sayi1:Int,sayi2: Int):Int=sayi1+sayi2
+}
